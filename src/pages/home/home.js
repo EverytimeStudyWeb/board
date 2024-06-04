bbc4e9186d44e2259ef2b22e425118634e69e2b4
jungmyeon <zhzsks123@daum.net>
2024-06-04 23:07:56 +0900

    board-4

---
+++ src/pages/home/home.js
@@ -0,0 +1,43 @@
+import { Link, NavLink } from 'react-router-dom'
+import './home.css'
+import Free from '../free/free'
+import { useEffect, useState } from 'react';
+import { Calendar } from 'react-calendar';
+
+
+export default function Home()
+{
+
+    const [date, setDate] = useState(new Date());
+    const tileClassName = ({date, view})=>{
+        if(date.toDateString() === new Date().toDateString()){
+            return 'today'
+        }
+        return '';
+    }
+
+    
+    
+    return(
+        <div className='home'>
+            <div className='board-container'>
+            <ul className='board'>
+                <p>게시판</p>  
+                <li><Link to={'/free'}>자유게시판</Link></li>
+                <li><Link to={'/market'}>중고장터</Link></li>
+                <li><Link to={'/timer'}>타이머</Link></li>
+            </ul>
+            <div className='calendar'>
+                <Calendar onChange={setDate} value={date} tileClassName={tileClassName}  />
+            </div>
+            </div>
+            <ul className='class'>
+                <p>시간표</p>
+            </ul>
+            <ul className='menu'>    
+                <p>학식</p>
+            </ul>
+        </div>
+    )
+}
+
